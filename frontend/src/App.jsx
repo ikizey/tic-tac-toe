0b1aa94073a9633878ba9439a082f51c46b61988
kizey <ikizey@icloud.com>
2022-12-25 13:13:11 +0400

    add Game Home Queue pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Queue from './pages/Queue';
+import Game from './pages/Game';
+
 function App() {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route path='/'>
+        <Route index element={<Home />} />
+        <Route path='queue' element={<Queue />} />
+        <Route path='game' element={<Game />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
