c79481b77d2e3b4545d08df79ceaafd377817b24
kizey <ikizey@icloud.com>
2022-12-26 01:44:27 +0400

    add TODO comment

--- frontend/src/pages/Lobby.jsx
+++ frontend/src/pages/Lobby.jsx
@@ -1,4 +1,12 @@
-import { Button, Container, HStack, Text, VStack } from '@chakra-ui/react';
+import {
+  Button,
+  Center,
+  Container,
+  HStack,
+  Stack,
+  Text,
+  VStack,
+} from '@chakra-ui/react';
 import React from 'react';
 import useLobby from '../hooks/useLobby';
 
@@ -7,7 +15,8 @@ const Lobby = () => {
 
   return (
     <Container centerContent>
-      <VStack spacing={5}>
+      <VStack align='center' spacing={5} height='100vh'>
+        //TODO ** for some reason it's not centered vertically
         <HStack spacing={5}>
           <Text>Total players connected: {totalPlayers}</Text>
           <Text>Total game sessions: {totalGames}</Text>
