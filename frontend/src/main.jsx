397023ebb174df35efb634a33f3939d5fd2e12fe
kizey <ikizey@icloud.com>
2022-12-25 22:15:17 +0400

    complete Home component

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react';
 import App from './App';
+import PlayerContextProvider from './store/playerContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <PlayerContextProvider>
+      <BrowserRouter>
+        <ChakraProvider>
+          <App />
+        </ChakraProvider>
+      </BrowserRouter>
+    </PlayerContextProvider>
   </React.StrictMode>
 );
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,62 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import {
+  Button,
+  Container,
+  FormControl,
+  FormErrorMessage,
+  FormHelperText,
+  FormLabel,
+  Heading,
+  Input,
+  VStack,
+} from '@chakra-ui/react';
+import { PlayerContext } from '../store/playerContext';
 
 const Home = () => {
-  return <div>Home</div>;
+  const { playerName, changePlayerName, playerHasValidName } =
+    useContext(PlayerContext);
+  const navigate = useNavigate();
+
+  const goQueue = () => {
+    navigate('/queue');
+  };
+
+  return (
+    <Container centerContent>
+      <VStack spacing={5}>
+        <Heading as='h2' size='xl' noOfLines={1}>
+          Welcome to Tic Tac Toe game!
+        </Heading>
+        <FormControl isInvalid={!playerHasValidName()} isRequired>
+          <FormLabel as='legend'>Enter your name</FormLabel>
+          <Input
+            placeholder='enter your name'
+            size='lg'
+            value={playerName}
+            onChange={({ target }) => changePlayerName(target.value)}
+          />
+          {playerHasValidName() ? (
+            <FormHelperText>
+              name should be between 3 to 20 characters.
+            </FormHelperText>
+          ) : (
+            <FormErrorMessage>
+              name should be between 3 to 20 characters.
+            </FormErrorMessage>
+          )}
+        </FormControl>
+        <Button
+          isDisabled={!playerHasValidName()}
+          colorScheme='purple'
+          size='lg'
+          onClick={goQueue}
+        >
+          Enter Game
+        </Button>
+      </VStack>
+    </Container>
+  );
 };
 
 export default Home;
