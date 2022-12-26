eddbea02e2252a40614998ee5055136ebb381338
kizey <ikizey@icloud.com>
2022-12-26 23:46:43 +0400

    complete Grid component

--- frontend/src/components/Grid.jsx
+++ frontend/src/components/Grid.jsx
@@ -1,5 +1,24 @@
-const Grid = () => {
-  return <div>Grid</div>;
+import { Grid as cGrid, GridItem } from '@chakra-ui/react';
+import Mark from './Mark';
+
+const Grid = ({ moves, makeMove }) => {
+  return (
+    <cGrid templateColumns='repeat(3, 1fr)' gap={6}>
+      {moves.map((move, index) => (
+        <GridItem
+          key={index}
+          w='100%'
+          h='20'
+          bg='grey.100'
+          onClick={() => {
+            move === '' && makeMove(index);
+          }}
+        >
+          <Mark move={move} />
+        </GridItem>
+      ))}
+    </cGrid>
+  );
 };
 
 export default Grid;
