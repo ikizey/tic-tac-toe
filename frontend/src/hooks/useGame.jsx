f86fa3f690daa066ee3f4e2ecbec24423d6733a0
kizey <ikizey@icloud.com>
2022-12-26 20:40:20 +0400

    add useGame hook

---
+++ frontend/src/hooks/useGame.jsx
@@ -0,0 +1,93 @@
+import { useContext, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { socket } from '../socket.config';
+import { PlayerContext } from '../store/playerContext';
+
+const CLIENT_EVENT = Object.freeze({
+  MOVE: 'move',
+  CONCEDE: 'concede',
+});
+
+const SERVER_EVENT = Object.freeze({
+  INTRODUCTION: 'introduction',
+  TURN: 'turn',
+  MOVES: 'moves',
+  GAME_OVER: 'gameOver',
+});
+
+export const useGame = () => {
+  const { playerUid, playerName } = useContext(PlayerContext);
+
+  const [opponentName, setOpponentName] = useState('unknown');
+  const [opponentID, setOpponentID] = useState('unknown');
+  const [isWinner, setIsWinner] = useState(undefined);
+  const [moves, setMoves] = useState([''.repeat(8)]);
+  const [isPlayerTurn, setIsPlayerTurn] = useState(false);
+
+  const navigate = useNavigate();
+
+  const reset = () => {
+    setOpponentName('unknown');
+    setOpponentID('unknown');
+    setIsWinner(undefined);
+    setMoves([''.repeat(8)]);
+    setIsPlayerTurn(false);
+  };
+
+  const makeMove = (index) => {
+    socket.emit(CLIENT_EVENT.MOVE, { index });
+  };
+
+  const concede = () => {
+    socket.emit(CLIENT_EVENT.CONCEDE, {});
+    navigate('/lobby');
+  };
+
+  useEffect(() => {
+    socket.on(
+      SERVER_EVENT.INTRODUCTION,
+      ({ player1Uid, player1Name, player2Uid, player2Name }) => {
+        reset();
+
+        if (player1Uid === playerUid) {
+          setIsPlayerTurn(true);
+          setOpponentName(player2Name);
+          setOpponentID(player2Uid);
+        } else {
+          setOpponentName(player1Name);
+          setOpponentID(player1Uid);
+        }
+      }
+    );
+
+    socket.on(SERVER_EVENT.TURN, ({ playerUid: pUid }) => {
+      setIsPlayerTurn(pUid === playerUid);
+    });
+
+    socket.on(SERVER_EVENT.MOVES, ({ moves }) => {
+      setMoves(moves);
+    });
+    socket.on(SERVER_EVENT.GAME_OVER, ({ playerUid: pUid }) => {
+      if (pUid === playerUid) {
+        setIsWinner(pUid === playerUid);
+      }
+    });
+
+    return () => {
+      socket.off(SERVER_EVENT.INTRODUCTION);
+      socket.off(SERVER_EVENT.TURN);
+      socket.off(SERVER_EVENT.MOVES);
+      socket.off(SERVER_EVENT.GAME_OVER);
+    };
+  });
+
+  return {
+    opponentName,
+    opponentID,
+    isWinner,
+    moves,
+    isPlayerTurn,
+    concede,
+    makeMove,
+  };
+};
