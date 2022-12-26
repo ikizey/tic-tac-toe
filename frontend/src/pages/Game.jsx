9dfee6901a7c05ff807c2bb952bcd81868939da8
kizey <ikizey@icloud.com>
2022-12-26 22:17:16 +0400

    add Game component

--- frontend/src/pages/Game.jsx
+++ frontend/src/pages/Game.jsx
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Container, Grid, VStack } from '@chakra-ui/react';
+import { useGame } from '../hooks/useGame';
+import { PlayerContext } from '../store/playerContext';
+import EndGame from '../components/EndGame';
+import GameStatus from '../components/GameStatus';
+import Grid from '../components/Grid';
+import Turn from '../components/Turn';
+import Vs from '../components/Vs';
 
 const Game = () => {
-  return <div>Game</div>;
+  const { playerName } = useContext(PlayerContext);
+  const {
+    opponentName,
+    isWinner,
+    moves,
+    isPlayerTurn,
+    concede,
+    leave,
+    makeMove,
+  } = useGame();
+  return (
+    <Container>
+      <VStack>
+        <GameStatus isWinner={isWinner} />
+        <Vs player1={playerName} player2={opponentName} />
+        <Turn
+          isPlayerTurn={isPlayerTurn}
+          playerName={playerName}
+          opponentName={opponentName}
+        />
+        <Grid />
+        <EndGame isWinner={isWinner} concede={concede} leave={leave} />
+      </VStack>
+    </Container>
+  );
 };
 
 export default Game;
