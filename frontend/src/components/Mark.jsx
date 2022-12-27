54f9ae177242e8bec543252db3007068cbca4570
kizey <ikizey@icloud.com>
2022-12-27 13:36:30 +0400

    fix show move

--- frontend/src/components/Mark.jsx
+++ frontend/src/components/Mark.jsx
@@ -2,7 +2,7 @@ import { Text } from '@chakra-ui/react';
 
 const Mark = ({ move }) => {
   const color = move === 'X' ? 'purple' : 'green';
-  return <Text colorScheme={color}>mark</Text>;
+  return <Text colorScheme={color}>{move}</Text>;
 };
 
 export default Mark;
